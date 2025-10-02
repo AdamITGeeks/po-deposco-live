7a3b05cedd4a35327b2ba49da7537fcbe1eba445
ramkumawat-ITG <ramkumawat@itgeeks.com>
2025-10-02 16:34:59 +0530

    orderId 2.0 error solve

--- app/routes/api/order/orderDeposco.js
+++ app/routes/api/order/orderDeposco.js
@@ -1,14 +1,10 @@
 import { json } from "@remix-run/node";
 import PurchaseOrder from "../../../models/purchase"; // apne path ke hisaab se import karo
- 
+
 export async function action({ request }) {
- 
   try {
     const payload = await request.json();
-  console.log(payload," console.log(payload)")
-  console.log(payload?.order?.[0]?.number,"paylodnumber")
-  console.log(payload?.order?.[0]?.number?.replace("PO_", ""),"paylodnumber")
-  const numberId = payload?.order?.[0]?.number?.replace("PO_", "") ;
+    const numberId = payload?.order?.[0]?.number?.replace("PO", "");
     const response = await fetch(
       "https://api.deposco.com/integration/RLL/orders/updates",
       {
@@ -20,22 +16,22 @@ export async function action({ request }) {
           Accept: "application/json",
         },
         body: JSON.stringify(payload),
-      }
+      },
     );
- 
+
     const data = await response.json();
- 
+
     if (response.ok && data) {
       // ✅ Agar API response success aata hai
       if (payload?.order?.[0]?.number?.replace("PO_", "")) {
         await PurchaseOrder.findOneAndUpdate(
-          { orderId: Number(numberId)},   // jo order bheja tha
-          { status: "Ordered" },          // status update
-          { new: true }
+          { orderId: Number(numberId) }, // jo order bheja tha
+          { status: "Ordered" }, // status update
+          { new: true },
         );
       }
     }
- 
+
     return json({ success: true, data });
   } catch (error) {
     return json({ success: false, error: error.message }, { status: 500 });
